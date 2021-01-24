da021c6a6840d28da801e389473e920f281cac1c
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2021-01-24 15:44:04 +0100

    bo Patient

--- src/com/app/muco/bo/Patient.java
+++ src/com/app/muco/bo/Patient.java
@@ -1,5 +1,69 @@
 package com.app.muco.bo;
 
 public class Patient extends Personne {
+	private int sante;
+	private String motCle;
+	private String commentaire;
+	
+	public Patient(int id, String nom, String prenom, String email, String motDePasse, String admin, int sante,
+			String motCle, String commentaire) {
+		super(id, nom, prenom, email, motDePasse, admin);
+		this.sante = sante;
+		this.motCle = motCle;
+		this.commentaire = commentaire;
+	}
 
+	public Patient( String nom, String prenom, String email, String motDePasse, int sante,
+			String motCle, String commentaire) {
+		super( nom, prenom, email, motDePasse);
+		this.sante = sante;
+		this.motCle = motCle;
+		this.commentaire = commentaire;
+	}
+	
+	public Patient( String nom, String prenom, String email, String motDePasse) {
+		super( nom, prenom, email, motDePasse);
+	}
+	
+	public Patient() {}
+
+	public int getSante() {
+		return sante;
+	}
+
+	public void setSante(int sante) {
+		this.sante = sante;
+	}
+
+	public String getMotCle() {
+		return motCle;
+	}
+
+	public void setMotCle(String motCle) {
+		this.motCle = motCle;
+	}
+
+	public String getCommentaire() {
+		return commentaire;
+	}
+
+	public void setCommentaire(String commentaire) {
+		this.commentaire = commentaire;
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [sante=" + sante + ", motCle=" + motCle + ", commentaire=" + commentaire + "]";
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
